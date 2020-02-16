52631546b58aac22fd56c1afbaca7940dbf09a9c
Seth Lessard <seth.lessard@pm.me>
2020-02-15 21:39:00 -0600

    Add styles

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,5 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components";
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, ownProps) => ({
+  color: ownProps.color || state.ui.primaryColor
+});
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const CB = styled.input`
+  position: relative;
+  top: -0.175em;
+  height: 1.25em;
+  width: 1.25em;
+  appearance: none;
+  -webkit-appearance: none;
+
+  margin: 0 1em 0 0;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:before {
+    position: absolute;
+    left: 0;
+    z-index: 1;
+    width: 1em;
+    height: 1em;
+    content: "";
+    border: 2px solid #022620;
+    transition: all 0.2s ease-in-out;
+    -webkit-transition: all 0.2s ease-in-out;
+    -moz-transition: all 0.2s ease-in-out;
+  }
+
+  &:checked:before {
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+    border: 3px solid ${props => props.color};
+    height: .5em;
+    border-top-style: none;
+    border-right-style: none;
+  }
+
+  &:checked {
+    background-color: unset;
+  }
+`;
+const Message = styled.span`
+  font-size: .8rem;
+  margin: 0 0 0 1em;
+  font-family: 'Roboto', sans-serif;
+`;
 
 class Checkbox extends Component {
   constructor(props) {
@@ -9,33 +67,24 @@ class Checkbox extends Component {
     }
   }
   render() {
-    const { children } = this.props;
+    const { children, color } = this.props;
     const style = {
-      checkbox: {
-        display: "flex",
-        alignItems: "center"
-      },
-      input: {
-
-      },
-      span: {
-        fontSize: ".8rem",
-        margin: "0 0 0 .5rem"
-      }
+      checkbox: {}
     };
     return (
-      <div style={style.checkbox}>
-        <input type="checkbox" style={style.input} placeholder="checkbox" />
+      <Wrapper style={style.checkbox}>
+        <CB color={color} type="checkbox" style={style.input} placeholder="checkbox" />
         { children &&
-        <span style={style.span}>{children}</span>
+        <Message style={style.span}>{children}</Message>
         }
-      </div>
+      </Wrapper>
     );
   }
 }
 
 Checkbox.propTypes = {
-  children: PropTypes.string
+  children: PropTypes.string,
+  color: PropTypes.string
 };
 
-export default Checkbox;
+export default connect(mapStateToProps)(Checkbox);
