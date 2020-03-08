338e6c64c95d512374eae5ad421011515da53807
Seth Lessard <seth.lessard@pm.me>
2020-03-08 01:51:58 -0600

    Fix example on mobile

--- example/src/fragment/CardFragment.js
+++ example/src/fragment/CardFragment.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Fragment from './Fragment';
 import { Example } from '../component';
-import { Paragraph, Card, Code, PaddedHeading, Table, TableHeader, TableRow, HeaderCell, TableBody, TableData, ScrollToTop, Text } from 'react-uix';
+import { Paragraph, Card, Code, PaddedHeading, Table, TableHeader, TableRow, HeaderCell, TableBody, TableData, ScrollToTop, Text, Grid } from 'react-uix';
 
 const EXAMPLE = `
 
@@ -69,31 +69,33 @@ const CardFragment = () => {
           The Card component is used to define an elevated surface.
         </Paragraph>
         <Example>
-          <Card z={1} style={style.card}>
-            <div style={style.center}>
-              <Text center>1</Text>
-            </div>
-          </Card>
-          <Card z={2} style={style.card}>
-            <div style={style.center}>
-              <Text center>2</Text>
-            </div>
-          </Card>
-          <Card z={3} style={style.card}>
-            <div style={style.center}>
-              <Text center>3</Text>
-            </div>
-          </Card>
-          <Card z={4} style={style.card}>
-            <div style={style.center}>
-              <Text center>4</Text>
-            </div>
-          </Card>
-          <Card z={5} style={style.card}>
-            <div style={style.center}>
-              <Text center>5</Text>
-            </div>
-          </Card>
+          <Grid columns={5}>
+            <Card z={1} style={style.card}>
+              <div style={style.center}>
+                <Text center>1</Text>
+              </div>
+            </Card>
+            <Card z={2} style={style.card}>
+              <div style={style.center}>
+                <Text center>2</Text>
+              </div>
+            </Card>
+            <Card z={3} style={style.card}>
+              <div style={style.center}>
+                <Text center>3</Text>
+              </div>
+            </Card>
+            <Card z={4} style={style.card}>
+              <div style={style.center}>
+                <Text center>4</Text>
+              </div>
+            </Card>
+            <Card z={5} style={style.card}>
+              <div style={style.center}>
+                <Text center>5</Text>
+              </div>
+            </Card>
+          </Grid>
         </Example>
 
         <Code text={EXAMPLE} />
