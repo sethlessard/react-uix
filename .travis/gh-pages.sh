621e2dbe8a681d16baf3da3ec07606d1446a8fb1
Seth Lessard <seth.lessard@pm.me>
2020-03-07 20:40:31 -0600

    Add gh-pages deploy script to travis

---
+++ .travis/gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+pushd example/
+
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+
+npm run build
+
+pushd build/
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push -f --quiet "https://${GITHUB_API_KEY}@github.com/sethlessard/react-uix.git" master:gh-pages > /dev/null 2>&1
+popd # build/
+popd # example/
