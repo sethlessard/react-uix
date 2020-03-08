75b40d820e7f8874ccb015da886b4a6852ae5b32
Seth Lessard <seth.lessard@pm.me>
2020-03-07 18:59:33 -0600

    Change to HashRouter to fix refresh issue with gh-pages

--- example/src/UIApp.js
+++ example/src/UIApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import styled from "styled-components";
 
 import {
