0169fbe39e6adefca44557ca34723af9b5038c74
Seth Lessard <sethlessard@outlook.com>
2020-10-04 01:43:35 -0500

    Fix switch update

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -37,8 +37,8 @@ class Switch extends Component {
     if (prevProps.checked !== checked) {
       if (this.ref && this.ref.current) {
         this.ref.current.checked = checked;
-        this.forceUpdate();
       }
+      this.forceUpdate();
     }
   }
 
